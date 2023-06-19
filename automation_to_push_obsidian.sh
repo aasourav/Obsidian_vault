040d073295812400e7f1d78e1120f49bdc340398
Ahsan Amin <ahsan.amin@headless.ltd>
2023-06-19 14:30:01 +0600

    06/19/23 02:30:01 PM

--- automation_to_push_obsidian.sh
+++ automation_to_push_obsidian.sh
@@ -3,6 +3,7 @@ path="/home/aasourav/Documents"
 cd "$path" || exit 1
 
 git add .
-git commit -m "$(date +'%Y-%m-%d')"
+#git commit -m "$(date +'%Y-%m-%d')"
+git commit -m "$(date +'%D %r')"
 git push origin main
 
